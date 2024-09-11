5afc09fff9e8c3f847f59b999082d3843274bf42
周悦 <17723471643@163.com>
2024-09-11 11:02:54 +0800

    init

---
+++ backend/src/main/java/com/hmdp/controller/UserController.java
@@ -0,0 +1,103 @@
+package com.hmdp.controller;
+
+
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.entity.UserInfo;
+import com.hmdp.service.IUserInfoService;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private IUserInfoService userInfoService;
+
+    /**
+     * 发送手机验证码
+     */
+    @PostMapping("code")
+    public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
+        // TODO 发送短信验证码并保存验证码
+        return Result.fail("功能未完成");
+    }
+
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
+        // TODO 实现登录功能
+        if(loginForm!=null){
+            String password = loginForm.getPassword();
+            String phone = loginForm.getPhone();
+
+            User user = new User();
+            user.setPhone(phone);
+            user.setPassword(password);
+            userService.save(user);
+
+            HashMap<String, Object> userForm = new HashMap<>();
+            userForm.put("phone",phone);
+            userForm.put("password",password);
+            JwtUtils jwtUtils = new JwtUtils();
+            String JwtPass = jwtUtils.genJwtPassword(userForm);
+            System.out.println(JwtPass);
+            return  Result.ok(JwtPass);
+        }
+
+        return Result.fail("功能未完成");
+    }
+
+    /**
+     * 登出功能
+     * @return 无
+     */
+    @PostMapping("/logout")
+    public Result logout(){
+        // TODO 实现登出功能
+        return Result.fail("功能未完成");
+    }
+
+    @GetMapping("/me")
+    public Result me(){
+        // TODO 获取当前登录的用户并返回
+        return Result.fail("功能未完成");
+    }
+
+    @GetMapping("/info/{id}")
+    public Result info(@PathVariable("id") Long userId){
+        // 查询详情
+        UserInfo info = userInfoService.getById(userId);
+        if (info == null) {
+            // 没有详情，应该是第一次查看详情
+            return Result.ok();
+        }
+        info.setCreateTime(null);
+        info.setUpdateTime(null);
+        // 返回
+        return Result.ok(info);
+    }
+}
